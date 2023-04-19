aad7e959618b7bb740d5cf96babd9f31ace12bd0
VSKuznec <sevactet.games@gmail.com>
2023-04-19 10:42:52 +0300

    initial commit

---
+++ UserProfile/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace UserProfile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
