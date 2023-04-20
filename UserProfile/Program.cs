826da3eb5e3748d5f7e460b8d3dbe47c639fa36d
VSKuznec <sevactet.games@gmail.com>
2023-04-20 19:35:11 +0300

    User Profile

--- UserProfile/Program.cs
+++ UserProfile/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace UserProfile
+﻿using System;
+
+internal class Program
 {
-    internal class Program
+    public static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        Console.WriteLine("Enter your name: ");
+        var name = Console.ReadLine();
+        Console.WriteLine("Enter your age");
+        var age = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Your name is {0} and age is {1}", name, age);
+
+        Console.WriteLine("Enter your birthdate: ");
+        var birthdate = Console.ReadLine();
+        Console.WriteLine("Your birthdate is {0}", birthdate);
+
+        Console.ReadKey();
     }
 }
