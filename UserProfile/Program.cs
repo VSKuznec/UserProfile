cfdc11e7e0bc67d568f701a05bd8ce84b075b42e
VSKuznec <sevactet.games@gmail.com>
2023-04-22 09:38:49 +0300

    Fixed the lines

--- UserProfile/Program.cs
+++ UserProfile/Program.cs
@@ -4,13 +4,13 @@ internal class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Enter your name: ");
+        Console.Write("Enter your name: ");
         var name = Console.ReadLine();
-        Console.WriteLine("Enter your age");
+        Console.Write("Enter your age: ");
         var age = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine("Your name is {0} and age is {1}", name, age);
 
-        Console.WriteLine("Enter your birthdate: ");
+        Console.Write("Enter your birthdate: ");
         var birthdate = Console.ReadLine();
         Console.WriteLine("Your birthdate is {0}", birthdate);
 
